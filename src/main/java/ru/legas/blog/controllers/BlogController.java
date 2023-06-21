2d542985f56f42543c6386d0942d149911012b09
Legas <00legas00@gmail.com>
2023-06-21 16:44:35 +0300

    Получаем посты из БД и выводим на страницу блога

--- src/main/java/ru/legas/blog/controllers/BlogController.java
+++ src/main/java/ru/legas/blog/controllers/BlogController.java
@@ -1,14 +1,21 @@
 package ru.legas.blog.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.legas.blog.models.Post;
+import ru.legas.blog.repositories.PostRepository;
 
 @Controller
+@AllArgsConstructor
 public class BlogController {
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model){
-        model.addAttribute("title", "Новости сайта");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 }
