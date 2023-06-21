356b6c0ab97b47469cc36d63418fef8a9f5dc5bb
Legas <00legas00@gmail.com>
2023-06-21 17:23:46 +0300

    Реализовал запись нового поста в БД

--- src/main/java/ru/legas/blog/controllers/BlogController.java
+++ src/main/java/ru/legas/blog/controllers/BlogController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.legas.blog.models.Post;
 import ru.legas.blog.repositories.PostRepository;
 
@@ -17,5 +19,23 @@ public class BlogController {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(
+            @RequestParam String title,
+            @RequestParam String anons,
+            @RequestParam String full_text,
+            Model model){
+
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
     }
 }
--- src/main/java/ru/legas/blog/models/Post.java
+++ src/main/java/ru/legas/blog/models/Post.java
@@ -5,15 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title, anons, full_text;
     private int views;
+
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 }
