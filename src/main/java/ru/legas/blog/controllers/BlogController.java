2ec35b8c437a207c15cbe98693a920117b1e7497
Legas <00legas00@gmail.com>
2023-06-21 19:28:39 +0300

    Реализовал изменение и удаление статей

--- src/main/java/ru/legas/blog/controllers/BlogController.java
+++ src/main/java/ru/legas/blog/controllers/BlogController.java
@@ -45,8 +45,7 @@ public class BlogController {
 
     @GetMapping("/blog/{id}")
     public String blogDetails(@PathVariable(value = "id") Long id, Model model){
-        if(!postRepository.existsById(id))
-            return "redirect:/blog";
+        if (isExistsPostById(id)) return "redirect:/blog";
 
         Optional<Post> optional = postRepository.findById(id);
         ArrayList<Post> res = new ArrayList<>();
@@ -54,5 +53,48 @@ public class BlogController {
         model.addAttribute("post", res);
 
         return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") Long id, Model model){
+        if (isExistsPostById(id)) return "redirect:/blog";
+
+        Optional<Post> optional = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        optional.ifPresent(res::add);
+        model.addAttribute("post", res);
+
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(
+            @PathVariable(value = "id") Long id,
+            @RequestParam String title,
+            @RequestParam String anons,
+            @RequestParam String full_text,
+            Model model){
+
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/remove")
+    public String blogPostDelete(@PathVariable(value = "id") Long id){
+
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+
+        return "redirect:/blog";
+    }
+
+    private boolean isExistsPostById(Long id) {
+
+        return !postRepository.existsById(id);
     }
 }
